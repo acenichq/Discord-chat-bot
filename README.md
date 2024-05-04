fa3fa1e647e5a5b346838d57828d3572da579ceb
Acenic <99246829+acenichq@users.noreply.github.com>
2024-05-04 17:21:58 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,51 @@
+# Discord-Chatbot
+An Open Source Chatbot for Discord | Discord.js | Made By Acê#1234
+> Powered By Brainshop
+
+## Brainshop 
+to get brainshop key, follow the steps below
+* Create an account on [brainshop.ai](https://brainshop.ai/)
+* Once you logged in, Click on Create Brain/ Add Brain
+* Click on "Create a Root Brain", Enter any Name
+ 
+#### do the same settings as shown below:
+![settings](https://cdn.discordapp.com/attachments/837202281618866207/837202384563994684/unknown.png)
+
+* Save the api details
+* Fill them in .env (this will keep your details private)
+
+## MongoDb
+to get mongoUrl, follow the steps below 
+* Create an account/Sign Up with Google on [MongoDb](https://www.mongodb.com/)
+* Create Free Cluster, name it anything
+* Copy the mongourl, put your password in it
+* go to network access and whitelist 0.0.0.0/0 ip to access it from anywhere
+* paste the url in .env
+
+### Bot Installation | Self Host
+* Extract `.zip` in a folder
+* Run `npm install`
+* Fill details (Bid, Key, Token)
+* Run `node index.js` in console
+
+### Bot Installition | Glitch/ Repl
+* Copy url of the repository
+* Go to [Glitch](https://glitch.com/)/ [Repl](https://replit.com/)
+* Click on `Import From Github`
+* Paste this url there
+* Wait till it imports
+* Fill the details in .env (Bid, Key, Token)
+* Start it
+
+### Detailed Installation
+[Youtube](https://www.youtube.com/channel/UCkpN5Xw5EMqoPsfQWKpYYwA)
+
+### [Support Server](https://discord.gg/ddEmXUPCE4)
+any error or problem while running the code? join the server for support  
+
+### Screenshot
+![Chatbot](https://cdn.discordapp.com/attachments/837202281618866207/837217805640138762/Capture.PNG)
+
+### Credits
+[Brainshop.ai](https://brainshop.ai/)
+
---
+++ index.js
@@ -0,0 +1,99 @@
+// .ENV
+require("dotenv").config();
+// Express | Ignore this if you're gonna self host
+const chalk = require('chalk');
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Server Online!') // Tell in comments 
+})
+
+app.listen(port, () => {
+    console.log(`${chalk.green(`Server online!`)}`)
+})
+
+// Code
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: true});
+client.brain = require('./util/chatSend');
+client.em = require("./util/embed")
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+// Database Mongo 
+/*
+const { Database } = require("quickmongo");
+client.db = new Database(process.env.mongoUrl);
+
+client.db.on("ready", () => {
+    console.log("   ==========================\n   Mongo Database connected!\n   ==========================");
+})
+*/
+// Database Quick.db
+client.db = require('quick.db');
+
+// Don't use both at same time or code will stop working
+
+// Ready
+client.on('ready', () => {
+  console.log(`${client.user.username} is Online`)
+setInterval(async () => {
+const statuses = [`.chat`, `Naitik.exe#3750`]
+   client.user.setActivity(statuses[Math.floor(Math.random() * statuses.length)], { type: "STREAMING", url: "https://www.youtube.com/channel/UCt5FBx5YaZ7QZmd4sDEJYmw"})
+}, 10000)
+});
+
+// Event Handler
+fs.readdir('./events/', (err, files) => { 
+    if (err) return console.error(err); 
+    files.forEach(file => {
+        const eventFunction = require(`./events/${file}`); 
+        if (eventFunction.disabled) return;
+
+        const event = eventFunction.event || file.split('.')[0]; 
+        const emitter = (typeof eventFunction.emitter === 'string' ? client[eventFunction.emitter] : eventFunction.emitter) || client;
+        const once = eventFunction.once; 
+
+        try {
+            emitter[once ? 'once' : 'on'](event, (...args) => eventFunction.run(...args, client)); 
+        } catch (error) {
+            console.error(error.stack); 
+        }
+    });
+});
+
+// Command Handler
+client.on("message", async message => {
+  if (message.author.bot || message.channel.type === "dm") return;
+// command 
+  let messageArray = message.content.split(" "),
+    cmd = messageArray[0].toLowerCase(),
+    args = messageArray.slice(1),
+    prefix = "."; // Add Prefix
+
+  if (!message.content.startsWith(prefix)) return;
+  let commandfile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)));
+  if (commandfile) commandfile.run(client, message, args);
+
+});
+
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.log(err);
+  let jsfile = files.filter(R => R.endsWith('.js'));
+  if (jsfile.length <= 0) {
+    return console.log(chalk.red("There are no commands"));
+  }
+  jsfile.forEach((f, i) => {
+    let pull = require(`./commands/${f}`);
+    console.log(`Loaded - ${f} | ${pull.config.aliases}`)
+
+    client.commands.set(pull.config.name, pull);
+if (pull.config.aliases) pull.config.aliases.forEach(alias => client.aliases.set(alias, pull.config.name))
+  });
+});
+// Login
+client.login(process.env.token); // Your Discord Bot Token
